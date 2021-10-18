b1c16ac7291e832eae2c274aaef45b0b9c756b57
Advikacoder <92722900+Advikacoder@users.noreply.github.com>
2021-10-18 15:46:17 +0530

    Add files via upload
    I can not put the green car image it is only showing as background.

--- main.js
+++ main.js
@@ -1,106 +1,110 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
-rover_width = 100;
-rover_height = 90;
+greencar_width = 50;
+greencar_height = 100;
 
-background_image = "mars.jpg";
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
 
-rover_image = "rover.png";
+greencar_x = 10;
+greencar_y = 10;
 
-rover_x = 10;
-rover_y = 10;
-
-function add()
-{
-    background_imgTag = new Image();
+function add() {
+	background_imgTag = new Image();
     background_imgTag.onload = uploadBackground;
     background_imgTag.src = background_image;
 
-    rover_imgTag = new Image();
-    rover_imgTag.onload = uploadBackground;
-    rover_imgTag.src = background_image;
+    greencar_imgTag = new Image();
+    greencar_imgTag.onload = uploadgreencar;
+    greencar_imgTag.src = background_image;
 }
 
-function uploadBackground()
-{
-    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+
 }
 
-function uploadrover()
-{
-    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+	
 }
+
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e) 
+function my_keydown(e)
 {
-    keyPressed = e.keyCode;
-    console.log(keyPressed);
-    if(keyPressed == '38')
-    {
-        uploadBackground();
-        console.log("up");
-    }
-    if(keyPressed == '40')
-    {
-        down();
-        console.log("down");
-    }
-    if(keyPressed == '37')
-    {
-        left();
-        console.log("left");
-    }
-    if(keyPressed == '39')
-    {
-        right();
-        console.log("right");
-    }
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
 }
 
 function up()
 {
-    if(rover_y >=0)
+    if(greencar_y >=0)
     {
-        rover_y = rover_y -10;
-        console.log("When up arrow is pressed, x = "+rover_x +" | y = " +rover_y)
+        greencar_y = greencar_y -10;
+        console.log("When up arrow is pressed, x = "+greencar_x +" | y = " +greencar_y)
         uploadBackground();
-        uploadrover();
+        uploadgreencar();
     }
 }
 
 function down()
 {
-    if(rover_y <=500)
+    if(greencar_y <=500)
     {
-        rover_y = rover_y +10;
-        console.log("When down arrow is pressed, x = "+rover_x +" | y = " +rover_y)
+        greencar_y = greencar_y +10;
+        console.log("When down arrow is pressed, x = "+greencar_x +" | y = " +greencar_y)
         uploadBackground();
-        uploadrover();
+        uploadgreencar();
     }
 }
 
 
 function left()
 {
-    if(rover_x >=0)
+    if(greencar_x >=0)
     {
-        rover_x = rover_x -10;
-        console.log("When left arrow is pressed, x = "+rover_x +" | y = " +rover_y)
+        greencar_x = greencar_x -10;
+        console.log("When left arrow is pressed, x = "+greencar_x +" | y = " +greencar_y)
         uploadBackground();
-        uploadrover();
+        uploadgreencar();
     }
 }
 
 function right()
 {
-    if(rover_x <=700)
+    if(greencar_x <=900)
     {
-        rover_x = rover_x +10;
-        console.log("When right arrow is pressed, x = "+rover_x +" | y = " +rover_y)
+        greencar_x = greencar_x +10;
+        console.log("When right arrow is pressed, x = "+greencar_x +" | y = " +greencar_y)
         uploadBackground();
-        uploadrover();
+        uploadgreencar();
     }
 }
